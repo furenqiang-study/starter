45f63b231704e82a8b7f31624eb620c09ba0378a
furenqiang <www.1726894668@foxmail.com>
2023-12-19 14:45:46 +0800

    集成服务指数监控actuator，编写方法调用次数demo代码

--- src/main/java/com/furenqiang/starter/robot/controller/RobotController.java
+++ src/main/java/com/furenqiang/starter/robot/controller/RobotController.java
@@ -4,6 +4,7 @@ import com.furenqiang.starter.robot.service.RobotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * @author lfy
@@ -21,4 +22,37 @@ public class RobotController {
         String s = robotService.sayHello();
         return s;
     }
+
+//    @GetMapping("/robot/http")
+//    public String http() {
+//        //获取响应完整信息
+//        WebClient client = WebClient.create("https://example.org");
+//
+//        Mono<ResponseEntity<Person>> result = client.get()
+//                .uri("/persons/{id}", id).accept(MediaType.APPLICATION_JSON)
+//                .retrieve()
+//                .toEntity(Person.class);
+//
+////只获取body
+//        WebClient client = WebClient.create("https://example.org");
+//
+//        Mono<Person> result = client.get()
+//                .uri("/persons/{id}", id).accept(MediaType.APPLICATION_JSON)
+//                .retrieve()
+//                .bodyToMono(Person.class);
+//
+////stream数据
+//        Flux<Quote> result = client.get()
+//                .uri("/quotes").accept(MediaType.TEXT_EVENT_STREAM)
+//                .retrieve()
+//                .bodyToFlux(Quote.class);
+//
+////定义错误处理
+//        Mono<Person> result = client.get()
+//                .uri("/persons/{id}", id).accept(MediaType.APPLICATION_JSON)
+//                .retrieve()
+//                .onStatus(HttpStatus::is4xxClientError, response -> ...)
+//        .onStatus(HttpStatus::is5xxServerError, response -> ...)
+//        .bodyToMono(Person.class);
+//    }
 }
--- src/main/java/com/furenqiang/starter/robot/service/RobotService.java
+++ src/main/java/com/furenqiang/starter/robot/service/RobotService.java
@@ -1,6 +1,9 @@
 package com.furenqiang.starter.robot.service;
 
 import com.furenqiang.starter.robot.properties.RobotProperties;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +13,21 @@ import org.springframework.stereotype.Service;
  * @create 2023-04-27 19:58
  */
 @Service
+@Slf4j
 public class RobotService {
 
     @Autowired
     RobotProperties robotProperties;
 
+    Counter counter;
+
+    public RobotService(MeterRegistry meterRegistry) {
+        counter = meterRegistry.counter("访问次数");
+    }
+
     public String sayHello() {
+        log.info("执行了{}方法", "sayHello");
+        counter.increment();
         return "你好：名字：【" + robotProperties.getName() + "】;年龄：【" + robotProperties.getAge() + "】" + "[邮箱]：" + robotProperties.getEmail();
     }
 }
